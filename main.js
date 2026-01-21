5ad43e617bf1bb5dbdf7f059431c5279f51277a5
talantovking075 <_EMAIL__GITHUB>
2026-01-21 18:49:53 +0600

    last

---
+++ main.js
@@ -0,0 +1,13 @@
+const fileInput = document.querySelector('.file-input');
+const imagePreview = document.querySelector('.image-preview');
+
+fileInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            imagePreview.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+});
